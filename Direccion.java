48a00d8f07b067f3870ebef08cc92f98360d55c8
jcvalencia <jvalencia@unam.mx>
2021-05-13 22:23:06 -0500

    Clase Direccion
    Creada a partir de los datos de dirección de la clase Propietario

---
+++ Direccion.java
@@ -0,0 +1,48 @@
+public class Direccion {
+	private String calle;
+	private String colonia;
+	private String ciudad;
+	private String estado; 
+	private String CP;
+	
+	public String getCalle() {
+		return calle;
+	}//getCalle
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}//setCalle
+
+	public String getColonia() {
+		return colonia;
+	}//getColonia
+
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}//setColonia
+
+	public String getCiudad() {
+		return ciudad;
+	}//getCiudad
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}//setCiudad
+
+	public String getEstado() {
+		return estado;
+	}//getEstado
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}//setEstado
+
+	public String getCP() {
+		return CP;
+	}//getCP
+
+	public void setCP(String CP) {
+		this.CP = CP;
+	}//setCP
+	
+}//class Direccion
