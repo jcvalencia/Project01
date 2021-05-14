0e035b20b3d2642de26392ed5d0ec697eb8a489a
jcvalencia <jvalencia@unam.mx>
2021-05-14 00:12:33 -0500

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,26 +1,44 @@
+
 public class Carro {
+
 	private String modelo;
 	private String color;
 	private int year;
-	private String marca;
+	private String marca; 
 	private String numeroSerie;
 	private Propietario propietario;
-	private int velocidadMaxima;
-	private int velocidadActual;
+	private int velocidadMaxima; // Kilómetros por hora
+	private int velocidadActual; // Kilómetros por hora
 	private int numeroPuertas;	
 	private boolean techoSolar;
-	private int numeroVelocidades;
+	private int numeroVelocidades; // Número total de velocidades
+	private int numeroVelocidadActual; // Número actual de velocidad
 	private boolean automatico;
-	private int capacidadCombustible;
+	private int capacidadCombustible; // litros
 
+	public String autonomiaViaje(double kilometros) {
+		return  String.format("%,.2f", capacidadCombustible/kilometros);
+	}// autonomiaViaje
+	
+		
 	//Método reduce la marcha
-	public void reduce() {
-		System.out.println("reduce Velocidad");
+	public void reduceNumeroVelocidad() {
+		if ((numeroVelocidadActual>0)) {
+			numeroVelocidadActual--;
+			System.out.println("reduce Velocidad -> " + numeroVelocidadActual);
+		} else if ((numeroVelocidadActual==0) && (velocidadActual==0)){
+			System.out.println("reduce Velocidad -> " + numeroVelocidadActual + " (reversa)");
+		} else {
+			System.out.println("No es posible bajar la cambiar la velocidad");	
+		}//else // if >0
 	}// reduce
 	
 	//Método de cambio de velocidades;
-	public void cambioVelocidad() {
-		System.out.println("cambio de Velocidad");
+	public void cambioNumeroVelocidad() {
+		if (numeroVelocidadActual<numeroVelocidades) {
+			numeroVelocidadActual++;
+		}// if <
+		System.out.println("Cambio de Velocidad -> " + numeroVelocidadActual);
 	}//cambioVelocidad
 
 	//Método de freno para el automóvil - Velocidad = 0 km / h;
@@ -31,10 +49,32 @@ public class Carro {
 	
 	//Método de aceleración que aumenta la velocidad de 1 a 1 km/h
 	public void acelerar() {
-		velocidadActual++;
+		if(velocidadActual<velocidadMaxima) {
+			velocidadActual++;	
+		}// Actual < Maxima
 		System.out.println("Velocidad Actual: " + velocidadActual);
 	}// acelerar
 	
+	public Carro(String modelo, String color, int year, String marca, String numeroSerie, Propietario propietario,
+			int velocidadMaxima, int velocidadActual, int numeroPuertas, boolean techoSolar, int numeroVelocidades,
+			int numeroVelocidadActual, boolean automatico, int capacidadCombustible) {
+		super();
+		this.modelo = modelo;
+		this.color = color;
+		this.year = year;
+		this.marca = marca;
+		this.numeroSerie = numeroSerie;
+		this.propietario = propietario;
+		this.velocidadMaxima = velocidadMaxima;
+		this.velocidadActual = velocidadActual;
+		this.numeroPuertas = numeroPuertas;
+		this.techoSolar = techoSolar;
+		this.numeroVelocidades = numeroVelocidades;
+		this.numeroVelocidadActual = numeroVelocidadActual;
+		this.automatico = automatico;
+		this.capacidadCombustible = capacidadCombustible;
+	}//Constructor
+
 	public String getModelo() {
 		return modelo;
 	}//getModelo
@@ -62,15 +102,15 @@ public class Carro {
 	public String getNumeroSerie() {
 		return numeroSerie;
 	}//getNumero_de_serie
-	public void setNumero_de_serie(String numeroSerie) {
+	public void setNumeroSerie(String numeroSerie) {
 		this.numeroSerie = numeroSerie;
 	}//setNumero_de_serie
 	public Propietario getPropietario() {
 		return propietario;
-	}//getDueno
-	public void setpropietario(Propietario propietario) {
+	}//getPropietario
+	public void setPropietario(Propietario propietario) {
 		this.propietario = propietario;
-	}//setDueno
+	}//setPropietario
 	public int getVelocidadMaxima() {
 		return velocidadMaxima;
 	}//getVelocidad_maxima
@@ -101,12 +141,20 @@ public class Carro {
 	public void setNumeroVelocidades(int numeroVelocidades) {
 		this.numeroVelocidades= numeroVelocidades;
 	}//setNumeroVelocidades
+	public int getNumeroVelocidadActual() {
+		return numeroVelocidadActual;
+	}//getNumeroVelocidadActual
+	public void setNumeroVelocidadActual(int numeroVelocidadActual) {
+		this.numeroVelocidadActual = numeroVelocidadActual;
+	}//setNumeroVelocidadActual
 	public boolean isAutomatico() {
 		return automatico;
 	}//isAutomatico
 	public void setAutomatico(boolean automatico) {
 		this.automatico = automatico;
 	}//setAutomatico
+
+	///Método para mostrar el volumen de combustible
 	public int getCapacidadCombustible() {
 		return capacidadCombustible;
 	}// getCapacidadCombustible
